5a643bd58986808496dbe2907f3ffd838126facb
Jojanny Pombo <jojannyp@weminnovacion.com>
2024-05-29 18:31:07 -0500

    se agrega script

---
+++ Script/table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [Pacientes](
+	[Id] INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+	[Name] VARCHAR(250) NOT NULL,
+	[Date] DATETIME NOT NULL,
+	[Email] VARCHAR(250) NOT NULL,
+	[Gender] Int ,
+	[Address] VARCHAR(250) NOT NULL,
+	[Phone] Int ,
+	[DateA] DATETIME NOT NULL,
+	[Remove] BIT NOT NULL DEFAULT 0,
+);
